e46f32bedcaa051615b2328467a1575cb352522d
macnaito <hiroki.naito18@gmail.com>
2026-07-16 02:08:53 +0900

    commit

--- Ar_fcc/scale/arfcc.f
+++ Ar_fcc/scale/arfcc.f
@@ -1,15 +1,15 @@
       program Arfcc
       implicit none
-      integer nmax,n,i,j,k,l,inc,m,bs,rec,aa
-      real*8 bohr,sgm,amass,ekin,Tk,a
+      integer nmax,n,i,j,k,l,inc,m,bs,rec
+      real*8 bohr,sgm,amass
       parameter (nmax=800000)
       parameter(bohr=0.5292d0)
       parameter(sgm=3.4d0/bohr) 
       parameter(amass=40d0*1836d0)
-      real*8 hxx,hyy,hzz,tempK,sgmv,sgmvv
-      real*8 x(3*nmax),v(3*nmax)
-      real*8 xp(4),yp(4),zp(4),cunit
-
+      real*8 hxx,hyy,hzz,tempK
+      real*8 x(3*nmax),v(3,nmax)
+      real*8 xp(4),yp(4),zp(4),cunit,treg,kb,g(3)
+      real*8 kinetic_energy
       cunit=2.0d0**(1d0/6)*sgm*
      &  sqrt(2d0)*0.975d0
       rec=0
@@ -54,78 +54,26 @@
       write(*,*)n
 
 ! 初速度      
+      treg=100.d0
+      kb=1.d0/11605.d0/27.2116d0
       do i=1,n
-        v(3*i-2)=0d0
-        v(3*i-1)=0d0
-        v(3*i  )=0d0
+         call gauss(g)
+         v(:,i)=sqrt(kb*treg/amass)*g(:)
       enddo
-      rec=0
-      if (rec==1) then
-       open(10,file='final50x.dat')
-       do i=1,3*n
-            read(10,*)x(i)
-       enddo
-       close(10)
-       open(11,file='final50v.dat')
-       do i=1,3*n
-            read(11,*)v(i)
+      kinetic_energy=0.d0
+      do i=1,n
+       kinetic_energy=kinetic_energy+0.5d0*amass*sum(v(:,i)**2)
       enddo
-      close(11)
+       tempK=kinetic_energy*2.d0/(3.d0*n)*11605.d0*27.2116d0
+      write(*,*)tempK     
+!
 
-!格子欠陥
-      do j=1,20
-       if (j==1) exit
-       call random_number(a)
-       aa=int(a*n)+1
-       write(*,*)aa
-       do i=aa,n-1
-            x(3*i-2)=x(3*(i+1)-2)
-            x(3*i-1)=x(3*(i+1)-1)
-            x(3*i  )=x(3*(i+1)  )
-            v(3*i-2)=v(3*(i+1)-2)
-            v(3*i-1)=v(3*(i+1)-1)
-            v(3*i  )=v(3*(i+1)  )
-       enddo
-       n=n-1
-      enddo
-
-!重心速度をひく     
-      sgmv=0d0
-      sgmvv=0d0
-      do i=1,3*n
-            sgmv=sgmv+v(i)
-      enddo
-      write(*,*)sgmv
-      sgmv=sgmv/(3d0*n)
-      do i=1,3*n
-            v(i)=v(i)-sgmv
-      enddo
-      do i=1,3*n
-            sgmvv=sgmvv+v(i)
-      enddo
-      do i=1,3*n
-        v(i)=(50d0/50.52d0)**0.5*v(i)
-      enddo
-      write(*,*) sgmvv
-      end if
-!重心速度
-
-      ekin=0d0
-      do j=1,3*n
-          ekin=ekin+0.5d0*amass*v(j)**2
-      enddo
-      Tk=ekin*2d0/(3d0*dble(n))*
-     &    27.2116*11605d0
-      write(*,*) Tk
-
-
-      
       open(10,file='init.dat')
       write(10,*)n
       do i=1,n
-        write(10,'(a,i5,6e15.7)') 'Ar  ',i,
+        write(10,'(a,1x,i5,6e15.7)') 'Ar',i,
      &       x(3*i-2)*bohr,x(3*i-1)*bohr,x(3*i)*bohr,
-     &       v(3*i-2),v(3*i-1),v(3*i)
+     &       v(1,i),v(2,i),v(3,i)
       enddo
       write(10,'(3e24.15)')hxx,0d0,0d0
       write(10,'(3e24.15)')0d0,hyy,0d0
@@ -139,13 +87,30 @@
      &       'Lattice="',hxx,0.0,0.0,0.0,hyy,0.0,0.0,0.0,hzz,'" ',
      &       'Properties=species:S:1:id:I:1:pos:R:3:tempK:R:1'
            do m=1,n
-            tempK=amass/2*(v(3*m-2)**2+v(3*m-1)**2+v(3*m)**2)
+            tempK=amass/2*(v(1,m)**2+v(2,m)**2+v(3,m)**2)
      &        *27.2116*11605d0
             write(11,'(a2,1x,i5,4e15.7)') 'Ar',m,
      &       x(3*m-2)*bohr,x(3*m-1)*bohr,x(3*m)*bohr,tempK
           enddo
           close(11)
       end
+
+
+ ! maxwell分布
+      subroutine gauss(g)
+      implicit none
+      real*8 g(3),u1,u2
+      integer i
+
+      do i=1,3
+       call random_number(u1)
+       call random_number(u2)
+       if(u1<1d-12) u1=1d-12
+       g(i)=sqrt(-2.d0*log(u1))*cos(2.d0*3.14159265d0*u2)
+      enddo
+      return
+      end
+ !
 
 
 
--- Ar_fcc/scale/test.f
+++ Ar_fcc/scale/test.f
@@ -1,3 +1,5 @@
+!　スケールした座標でのMD 温度・圧力制御なし　エネルギー保存を確認
+!　速度こうしんはvで行う　
       program ljmd
       
       implicit real*8 (a-h,o-z)
@@ -5,21 +7,27 @@
       parameter(ev_k=11605.d0)
       parameter(hartree_ev=27.2116d0)
       parameter(kb=1.d0/ev_k/hartree_ev)
-      parameter(gpa=29421.d0)
+      parameter(Gp=29421.d0)
       parameter(bohr=0.5292d0)
 
       real*8,allocatable,dimension(:,:)::x,v,frc,s,ds
       real*8,allocatable,dimension(:)::mass
       character*8,allocatable,dimension(:)::lsp
       character*40 filename
-      real*8 p(3,3),h(3,3),vh(3,3),h_inver(3,3)
+      real*8 p(3,3),h(3,3),h_inver(3,3)
       real*8 G(3,3),e_kin(3,3),str(3,3)
-      integer neighbor_counter(13500),neighbor_list(300,13500)
 
       real*8,allocatable,dimension(:)::rec1,rec2,rec3,rec4,rec5,rec6
-      
+      real*8,allocatable,dimension(:)::gpa,smap
+
+      maxstep=1000
+      allocate(gpa(maxstep),smap(maxstep),rec1(maxstep),rec2(maxstep)
+     &        ,rec3(maxstep),rec4(maxstep),rec5(maxstep),rec6(maxstep))
+
+       k=0
+
 !　.datファイルの読み込み      
-      open(10,file='lv50_bs1d-4.dat')
+      open(10,file='init.dat')
        read(10,*)ntot
         allocate(x(3,ntot),v(3,ntot),frc(3,ntot),s(3,ntot),ds(3,ntot),
      &              mass(ntot),lsp(ntot))
@@ -35,8 +43,9 @@
       h=h/bohr
       x(:,:)=x(:,:)/bohr    !a.u.に変換
 !
+      dt=41*0.5d0
 
-      call ol_inverse(h,vol,h_inver)
+      call vol_inverse(h,vol,h_inver)
 
 !　スケール  
       do i=1,ntot
@@ -44,7 +53,7 @@
         ds(:,i)=matmul(h_inver,v(:,i))
       enddo 
 
-! 温度・圧力の計算
+! 温度の計算
       call cpu_time(t1)
       ekin=0.d0
       do i=1,ntot
@@ -102,13 +111,89 @@
       call cpu_time(t2)
       write(*,*)tempk,t2-t1
 !
-      call neighbor(s,ntot,h,neighbor_counter,neighbor_list)
-      call pot(ntot,s,h,str,frc,neighbor_list,neighbor_counter)
-      write(*,*)ntot,str(1,1)
+      call pot(f,frc,s,ntot,h,str)
 
+!=========================計算スタート====================
+      do istep=1,maxstep
+
+!　速度の更新
+      v=matmul(h,ds)
+      do i=1,ntot
+        v(:,i)=v(:,i)+0.5d0*dt*frc(:,i)/mass(i)
+        ds(:,i)=matmul(h_inver,v(:,i))
+      enddo
+!         
+!　位置の更新+pbc
+      do i=1,ntot
+        s(:,i)=s(:,i)+dt*ds(:,i)
+        do j=1,3
+          if(s(j,i)>=1.d0) s(j,i)=s(j,i)-1.d0
+          if(s(j,i)< 0.d0) s(j,i)=s(j,i)+1.d0
+        enddo
+      enddo
+! 
+      call pot(f,frc,s,ntot,h,str)
+
+!　速度の更新
+      v=matmul(h,ds)
+      do i=1,ntot
+        v(:,i)=v(:,i)+0.5d0*dt*frc(:,i)/mass(i)
+        ds(:,i)=matmul(h_inver,v(:,i))
+      enddo
+! 
+!　温度の計算
+      ekin=0.d0
+      do i=1,ntot
+       ekin=ekin+0.5d0*mass(i)
+     &  *(v(1,i)**2+v(2,i)**2+v(3,i)**2)
+      enddo
+      tempk=ekin*2.d0/3.d0/ntot/kb
+!
+      call pressure(istep,maxstep,v,ntot,str,vol,p,gpa,smap)
+
+!　xyzファイルの出力
+      filename='out000.xyz'
+      if(mod(istep,maxstep/100).eq.0)then
+      k=k+1
+      do i=1,ntot
+        x(:,i)=matmul(h,s(:,i))
+      enddo
+      write(filename(4:6),'(i3.3)')k
+      open(11,file=filename)
+      write(11,*)ntot
+      write(11,'(a,3(3e15.7),a,a)')
+     &       'Lattice="',h(1,1)*bohr,h(2,1)*bohr,h(3,1)*bohr,
+     &                   h(1,2)*bohr,h(2,2)*bohr,h(3,2)*bohr,
+     &                   h(1,3)*bohr,h(2,3)*bohr,h(3,3)*bohr,'" ',
+     &       'Properties=species:S:1:id:I:1:pos:R:3:tempK:R:1'
+      do m=1,ntot
+      write(11,'(a2,1x,i5,4e15.7)')lsp(m),m,
+     &       x(1,m)*bohr,x(2,m)*bohr,x(3,m)*bohr,0d0
+      enddo
+      close(11)
+      endif
+!
+
+      write(*,*)istep,tempk,gpa(istep),smap(istep),f+ekin
+      rec1(istep)=tempk
+      rec2(istep)=f
+      rec3(istep)=ekin
+      rec4(istep)=f+ekin
+      rec5(istep)=0.d0
+      rec6(istep)=0.d0
       
+      enddo
 
+!=====================loopここまで==============================
 
+!　グラフ
+      open(12,file='t.dat')
+      do i=1,maxstep
+        write(12,*)rec1(i),rec2(i),rec3(i),rec4(i),rec5(i),rec6(i)
+     &             ,gpa(i),smap(i)
+      enddo
+      close(12)  
+!
 
       end program
 
@@ -138,56 +223,53 @@
       return
       end
 !
-!　linked cell
-      subroutine neighbor
-     &   (s,ntot,h,neighbor_counter,neighbor_list)
-      implicit none
-      integer mx,my,mz,hx_lc,hy_lc,hz_lc,m1,m,ntot
-      real*8 h(3,3),s(3,ntot)
-      real*8 cutoff,rij2
-      integer hyz_lc,hxyz_lc,i,m1x,m1y,m1z
-      integer ishiftx,ishifty,ishiftz,j
-      integer kuxmax,kuymax,kuzmax,kux,kuy,kuz
-      integer,allocatable,dimension(:):: lshd,lscl
-      real*8 rij(3),hx_cell,hy_cell,hz_cell
-      integer max_neighbor
-      parameter (max_neighbor=200)
-      integer neighbor_counter(ntot),neighbor_list(max_neighbor,ntot)
-      parameter(cutoff=2.5d0*3.4d0/0.5292d0) !cutoff=16.06
+!　call pot
+      subroutine pot(f,frc,s,ntot,h,str)
+      implicit real*8 (a-h,o-z)
+      integer hx_lc,hy_lc,hz_lc,hxyz_lc,hyz_lc,pea
+      real*8 frc(3,ntot),s(3,ntot),h(3,3),str(3,3),rij(3)
+      parameter(sgm=3.4d0/0.5292d0)  
+      parameter(eps=120d0/11605d0/27.2116d0)
+      parameter(sgm12=sgm**12,sgm6=sgm**6)
+      parameter(cutoff=2.5d0*sgm)
+      integer,allocatable,dimension(:)::lshd,lscl
 
-       neighbor_counter=0
+      frc=0.d0
+      str=0.d0
+      pea=0
 
-       hx_lc=ceiling(sqrt(dot_product(h(:,1),h(:,1)))/cutoff)-1 !x座標のセル数
-       hy_lc=ceiling(sqrt(dot_product(h(:,2),h(:,2)))/cutoff)-1
-       hz_lc=ceiling(sqrt(dot_product(h(:,3),h(:,3)))/cutoff)-1
-       hyz_lc=hy_lc*hz_lc     !セルのyz平面の数
-       hxyz_lc=hyz_lc*hx_lc   !総セル数 
-       hx_cell=1.d0/hx_lc     !各方向のセルの長さ ボックスサイズは１
-       hy_cell=1.d0/hy_lc
-       hz_cell=1.d0/hz_lc
-       allocate(lshd(hxyz_lc),lscl(ntot))
-       lshd=0
-       do i=1,ntot
-         mx=int(s(1,i)/hx_cell)
-         my=int(s(2,i)/hy_cell)
-         mz=int(s(3,i)/hz_cell)
-         mx=min(max(mx,0),hx_lc-1)
-         my=min(max(my,0),hy_lc-1)
-         mz=min(max(mz,0),hz_lc-1)
-         m=mx*hyz_lc+my*hz_lc+mz+1
-         lscl(i)=lshd(m)
-         lshd(m)=i
-       enddo
+      hx_lc=ceiling(sqrt(dot_product(h(:,1),h(:,1)))/cutoff)-1 !x座標のセル数
+      hy_lc=ceiling(sqrt(dot_product(h(:,2),h(:,2)))/cutoff)-1
+      hz_lc=ceiling(sqrt(dot_product(h(:,3),h(:,3)))/cutoff)-1
+      hyz_lc=hy_lc*hz_lc     !セルのyz平面の数
+      hxyz_lc=hyz_lc*hx_lc
+      hx_cell=1.d0/hx_lc     !各方向のセルの長さ
+      hy_cell=1.d0/hy_lc
+      hz_cell=1.d0/hz_lc
+      allocate(lshd(hxyz_lc),lscl(ntot))
+      lshd=0
+      do i=1,ntot
+        mx=int(s(1,i)/hx_cell)
+        my=int(s(2,i)/hy_cell)
+        mz=int(s(3,i)/hz_cell)
+        mx=min(max(mx,0),hx_lc-1)
+        my=min(max(my,0),hy_lc-1)
+        mz=min(max(mz,0),hz_lc-1)
+        m=mx*hyz_lc+my*hz_lc+mz+1
+        lscl(i)=lshd(m)
+        lshd(m)=i
+      enddo
 
-       kuxmax=1
-       kuymax=1
-       kuzmax=1
+      kuxmax=1
+      kuymax=1
+      kuzmax=1
+      f=0d0
       
-       do mz=0,hz_lc-1
-       do my=0,hy_lc-1
-       do mx=0,hx_lc-1
+      do mz=0,hz_lc-1
+      do my=0,hy_lc-1
+      do mx=0,hx_lc-1
         m=mx*hyz_lc+my*hz_lc+mz+1 !3つのdoloopで全てのセルを走査
-        if (lshd(m)==0)cycle !空のセルはスキップ
+        if(lshd(m)==0) cycle !空のセルはスキップ
         do kuz=-kuzmax,kuzmax
         do kuy=-kuymax,kuymax
         do kux=-kuxmax,kuxmax
@@ -208,13 +290,20 @@
                 rij(:)=rij(:)-dnint(rij(:))
                 rij=matmul(h,rij)
                 rij2=rij(1)**2+rij(2)**2+rij(3)**2
-                if (rij2<(cutoff*1.2)**2) then
-                 neighbor_counter(i)=neighbor_counter(i)+1
-                 neighbor_counter(j)=neighbor_counter(j)+1
-                 neighbor_list(neighbor_counter(i),i)=j
-                 neighbor_list(neighbor_counter(j),j)=i
+                if (rij2<(cutoff)**2) then
+                  pea=pea+1
+                  f=f+4d0*eps*(sgm12/rij2**6-sgm6/rij2**3)
+                  factor=4d0*eps*
+     &            (-12d0*sgm12/rij2**7+6d0*sgm6/rij2**4)
+                  frc(:,i)=frc(:,i)-factor*rij(:)
+                  frc(:,j)=frc(:,j)+factor*rij(:)
+                  do k=1,3
+                  do l=1,3
+                   str(k,l)=str(k,l)-factor*rij(k)*rij(l)
+                  enddo
+                  enddo
                 endif
-              end if
+              endif
               j=lscl(j)
             enddo
             i=lscl(i)
@@ -229,56 +318,6 @@
       return
       end
 !
-! call pot      
-      subroutine pot(ntot,s,h,virial,dfdx,neighbor_list,neighbor_counter)
-      implicit none
-      integer ntot,j,k,l,a,b
-      real*8 sgm,eps,sgm12,sgm6,cutoff,rij(3),rij2
-      real*8 f,dfdx(3,ntot),factor,virial(3,3),s(3,ntot),h(3,3)
-      integer neighbor_counter(ntot),max_neighbor
-      parameter (max_neighbor=200)
-      integer neighbor_list(max_neighbor,ntot)
-
-      parameter(sgm=3.4d0/0.5292d0)
-      parameter(eps=120d0/11605d0/27.2116d0)
-      parameter(sgm12=sgm**12,sgm6=sgm**6)
-      parameter(cutoff=2.5d0*sgm)
-
-      dfdx=0d0
-      f=0d0
-      virial=0d0
-
-      do a=1,ntot
-      do b=1,neighbor_counter(a)
-       j=neighbor_list(b,a)
-       if (a<j)then
-       rij=s(:,a)-s(:,j)
-       rij=rij-dnint(rij)
-       rij=matmul(h,rij)
-       rij2=dot_product(rij,rij)
-       if (rij2<cutoff**2)then
-        f=f+4d0*eps*(sgm12/rij2**6-sgm6/rij2**3)
-        factor=4d0*eps*
-     &       (-12d0*sgm12/rij2**7+6d0*sgm6/rij2**4)
-        dfdx(1,a)=dfdx(1,a)-factor*rij(1)
-        dfdx(2,a)=dfdx(2,a)-factor*rij(2)
-        dfdx(3,a)=dfdx(3,a)-factor*rij(3)
-        dfdx(1,j)=dfdx(1,j)+factor*rij(1)
-        dfdx(2,j)=dfdx(2,j)+factor*rij(2)
-        dfdx(3,j)=dfdx(3,j)+factor*rij(3)
-        do k=1,3
-        do l=1,3
-         virial(k,l)=virial(k,l)-factor*rij(k)*rij(l)
-        enddo
-        enddo
-       endif
-       endif
-      enddo
-      enddo
-
-      return
-      end
-
 !subrutine ishift  
       subroutine get_ishift(lc,i,ishift)
       implicit none
@@ -299,10 +338,33 @@
       return
       end
 !
-  
- 
-      
+!　瞬間の圧力と平均の圧力
+      subroutine pressure(istep,maxstep,v,ntot,str,vol,p,gpa,smap)
+      implicit real*8 (a-g,o-z)
+      real*8 v(3,ntot),str(3,3),p(3,3),e_kin(3,3)
+      real*8 smap(maxstep),gpa(maxstep)
+      parameter(mass=40d0*1836d0)
+      parameter(n=500)
+      save sum
+      data sum/0.d0/
 
+      e_kin=mass*matmul(v,transpose(v))
+      do l=1,3
+      do m=1,3
+        p(l,m)=(e_kin(l,m)+str(l,m))/vol
+      enddo
+      enddo 
+      gpa(istep)=(p(1,1)+p(2,2)+p(3,3))/3.d0*29421.d0
 
+      if(istep<=n) then
+        sum=sum+gpa(istep)
+        smap(istep)=sum/istep
+      else 
+        sum=sum-gpa(istep-n)+gpa(istep)
+        smap(istep)=sum/n
+      endif
+
+      return
+      end
 
 
